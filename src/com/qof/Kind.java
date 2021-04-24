9ffb6aa9ea234c599fe242ce09293712f2f7e7db
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-24 23:36:51 +0200

    Aangepast Class Kind
    Omschrijving: implement functies gevuld met gelijk soorte functies zo als Baby met aanpassingen.  1 functie als het kind gaat buiten spelen om te kijken of het een aandoening op loopt.  2. functie praatOverProbleem nu het kind ouder is kan het soms aan de ouders communiceren dat het zich niet goed voelt retourneert boolean.

--- src/com/qof/Kind.java
+++ src/com/qof/Kind.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 public class Kind extends Persoon implements KwaliteitVanLeven{
     private final Leeftijdcategory leeftijdCategory;
-    private final double levensVerwachting = 51;
-    private final double levensKwaliteit = 10;
     private Set<Aandoeningen> aandoeningen = new HashSet<>();
 
     public Kind(String naam, String geslacht, int leeftijd, Leeftijdcategory leeftijdCategory) {
@@ -18,29 +16,138 @@ public class Kind extends Persoon implements KwaliteitVanLeven{
         this.aandoeningen.add(aandoeningen);
     }
 
+    public String buitenSpelen(Aandoeningen aandoeningen){
+        if (Math.random() < 0.5){
+            setAandoening(aandoeningen);
+            return getNaam() + ": Heeft tijdens buiten spelen een aandoening opgelopen: " + aandoeningen.getNaam();
+        }
+        return getNaam() + ": is buiten wezen spelen.";
+    }
+
+    private boolean praatOverProbleem(){
+        boolean praat = false;
+        if (getKwaliteitVanLeven() < 5){
+            if (Math.random() < 0.5){
+                praat = true;
+            }
+        }
+        return praat;
+    }
+
+    private boolean oudersMerkenOp(){
+        boolean oudersMerkenop = false;
+
+        if (Math.random() < 0.5){
+            return true;
+        }
+
+        if(praatOverProbleem()){
+            oudersMerkenop = true;
+        }
+        return oudersMerkenop;
+    }
+
+
     @Override
     public String toString() {
-        return null;
+        String samenvatting;
+        samenvatting = "Naam: " + getNaam() + "\nGeslacht: " + getGeslacht() + "\nlevend: " +  getLevend() + "\nLeeftijd: "+ getLeeftijd()  +  "\nLeeftijds Category: " +
+                leeftijdCategory.toString() + "\nVerwachte Levensverwachting: " +  getLeeftijdsverwachting() + " jaar."+ "\n"+
+                "Verwachte Levens kwaliteit: " + getKwaliteitVanLeven() + getAandoeningen();
+        return samenvatting;
     }
 
     @Override
     public String getAandoeningen() {
-        return null;
+        String Aandoeningen = "";
+        if (aandoeningen.isEmpty()){
+            Aandoeningen += "\nHeeft de geen aandoeningen\n";
+            return Aandoeningen;
+        }
+        Aandoeningen +=  "\nTotaal aantal aandoeningen: " + aandoeningen.size() + "\nHeeft de volgende aandoeningen:\n";
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            Aandoeningen += aandoeningen.toString() + "\n";
+        }
+        return Aandoeningen;
     }
 
     @Override
-    public String getLeeftijdsverwachting() {
-        return null;
+    public double getLeeftijdsverwachting() {
+        double levensVerwachting = 80;
+        double leeftijdsverwachting = levensVerwachting;
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            leeftijdsverwachting -= aandoeningen.getVerkortingLevensverwachting();
+        }
+        if (leeftijdsverwachting < 0){
+            return 0;
+        }
+        return (int)leeftijdsverwachting;
     }
 
     @Override
-    public String getKwaliteitVanLeven() {
-        return null;
+    public double getKwaliteitVanLeven() {
+        double levensKwaliteit = 10;
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            levensKwaliteit = (levensKwaliteit - (levensKwaliteit - aandoeningen.getLevensKwaliteit()));
+        }
+
+        return Math.round(levensKwaliteit * 100.0) / 100.0;
     }
+
+    @Override
+    public double getZichtbaarheid() {
+        double zichtbaar = 0;
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            zichtbaar += aandoeningen.getZichtbaarheid();
+        }
+        return zichtbaar;
+    }
+
+    @Override
+    public boolean getErnst() {
+        boolean Ernst = false ;
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            if (aandoeningen.getDoodelijkheid()) {
+                Ernst = true;
+                break;
+            }
+        }
+        return Ernst;
+    }
+
 
     @Override
     public String Yearpassed() {
-        return null;
+        if (getLevend()){
+            setLeeftijd(getLeeftijd() + 1);
+            if (getErnst()){
+
+                if (oudersMerkenOp()){
+                    return getNaam() + ": is naar het ziekenhuis gebracht.";
+                }
+
+                if (getZichtbaarheid() > 5){
+                    return getNaam() + ": is naar het ziekenhuis gebracht.";
+                }
+                setLevend(false);
+                return getNaam() + ": Is helaas overleden";
+            }
+
+            if (getLeeftijdsverwachting() == getLeeftijd()){
+                return  getNaam() + ": Is overleden op een" + getLeeftijd() + "jarige leeftijd.";
+            }
+
+            return getNaam() + ": Heeft het 1 jaar langer overleefd.";
+        }
+        return "";
+
+    }
+
+    public String equals(Kind kind) {
+        if (this == kind){
+            return "Dit is het zelfde kind";
+        }
+        return "Dit is niet het zelfde kind";
     }
 
 
