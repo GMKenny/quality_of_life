d83a4b240cf096bcd6109357d310ff0d76016b54
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-24 15:48:43 +0200

    Aangepast Class Baby variablen en 2 functies toegevoegd
    Omschrijving: Er zijn 2 variable aan toegevoegd levensverwachting en levenskwaliteit. Functie leeftijdsverwachting retourneerd de verwachte leeftijd gebaseerd op de verschilende aandoeningen.
    Functie kwaliteitvanleven geeft de verwachte kwaliteit van leven terug.

--- src/com/qof/Baby.java
+++ src/com/qof/Baby.java
@@ -6,7 +6,8 @@ import java.util.Set;
 
 public class Baby extends Persoon implements KwaliteitVanLeven{
     private final Leeftijdcategory leeftijdCategory;
-    private double levensVerwachting = 81;
+    private final double levensVerwachting = 85;
+    private final double levensKwaliteit = 10;
     private final Set<Aandoeningen> aandoeningen = new HashSet<>();
 
     public Baby(String naam, String geslacht, int leeftijd, Leeftijdcategory leeftijdCategory) {
@@ -18,32 +19,52 @@ public class Baby extends Persoon implements KwaliteitVanLeven{
         this.aandoeningen.add(newAandoeningen);
     }
 
+
     @Override
-    public String getAllString() {
+    public String toString() {
         String samenvatting;
-        samenvatting = "Naam: " + getNaam() + "\nGeslacht: " + getGeslacht() + "\nLeeftijds Category: " + leeftijdCategory.toString() + "\n";
+        samenvatting = "Naam: " + getNaam() + "\nGeslacht: " + getGeslacht() + "\nLeeftijds Category: " +
+                leeftijdCategory.toString() + "\n" +  getLeeftijdsverwachting() + "\n"+  getKwaliteitVanLeven() + getAandoeningen();
         return samenvatting;
     }
 
     @Override
-    public String getAandoeningenString() {
-        String Aandoeningen = "Naam: " + getNaam()  + "\nLeeftijd: " + getLeeftijd();
+    public String getAandoeningen() {
+        String Aandoeningen = "";
         if (aandoeningen.isEmpty()){
             Aandoeningen += "\nHeeft de geen aandoeningen\n";
             return Aandoeningen;
         }
-        Aandoeningen +=  "\nHeeft de volgende aandoeningen:\n";
+        Aandoeningen +=  "\nTotaal aantal aandoeningen: " + aandoeningen.size() + "\nHeeft de volgende aandoeningen:\n";
         for ( Aandoeningen aandoeningen : aandoeningen){
-            Aandoeningen += aandoeningen.toString();
+            Aandoeningen += aandoeningen.toString() + "\n";
         }
         return Aandoeningen;
     }
 
     @Override
-    public String getLeeftijdsverwachtingString() {
-        String Aandoeningen = "Naam: " + getNaam();
+    public String getLeeftijdsverwachting() {
+        double leeftijdsverwachting = this.levensVerwachting - getLeeftijd();
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            leeftijdsverwachting -= aandoeningen.getVerkortingLevensverwachting();
+        }
+        return "Verwachte Leeftijdverwachting: " + (int)leeftijdsverwachting;
+    }
 
+    @Override
+    public String getKwaliteitVanLeven() {
+        double levensKwaliteit = this.levensKwaliteit;
+        for ( Aandoeningen aandoeningen : aandoeningen){
+            levensKwaliteit = (levensKwaliteit - (levensKwaliteit - aandoeningen.getLevensKwaliteit()));
+        }
 
+        return "Verwachte Levens kwaliteit: " + Math.round(levensKwaliteit * 100.0) / 100.0;
+    }
+
+    @Override
+    public String Yearpassed() {
         return null;
     }
+
+
 }
