74c9c2b65394e743b0808e733016c9fce447018c
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-24 23:47:08 +0200

    Main Aangepast.
    Omschrijving: Main aangepast zodat die de functionaliteit test.

--- src/com/qof/Main.java
+++ src/com/qof/Main.java
@@ -3,25 +3,41 @@ package com.qof;
 public class Main {
 
     public static void main(String[] args) {
-        Baby een = new Baby("Asta","Man", 1, Leeftijdcategory.Baby);
-        Baby twee = new Baby("Amber","Vrouw", 1, Leeftijdcategory.Baby);
+        Baby babyEen = new Baby("Asta","Man", 1, Leeftijdcategory.Baby);
+        Baby babyTwee = new Baby("Amber","Vrouw", 1, Leeftijdcategory.Baby);
+        Kind kindEen = new Kind( "Kees", "Man", 1, Leeftijdcategory.Kind);
+        Puber puberEen = new Puber("Clara", "vrouw", 17, Leeftijdcategory.Puber);
+        Puber puberTwee = new Puber("Jan", "Man", 18, Leeftijdcategory.Puber);
 
-        System.out.println(een.toString());
 
-        Aandoeningen Autisme = new Aandoeningen("Autisme", 16, 7.5, 7, false);
-        Aandoeningen Hazenlip = new Aandoeningen("Hazenlip", 0, 9.99, 10, false);
-        Aandoeningen Hersenvliesontsteking = new Aandoeningen("Hersenvliesontsteking", 90, 2, 4, true);
+        Aandoeningen autisme = new Aandoeningen("Autisme", 16, 7.5, 7, false, false);
+        Aandoeningen hazenlip = new Aandoeningen("Hazenlip", 0, 9.99, 10, false, false);
+        Aandoeningen hersenvliesontsteking = new Aandoeningen("Hersenvliesontsteking", 90, 2, 4, true, false);
+        Aandoeningen rinovirus = new Aandoeningen("Rinovirus", 0, 8, 8, false, false);
+        Aandoeningen chlamydia = new Aandoeningen("Chlamyda", 0, 7, 7, false, true);
+        Aandoeningen trichomonas = new Aandoeningen( "Trichomonas", 0,7, 6, false, true);
 
-        een.setAandoening(Hersenvliesontsteking);
-        twee.setAandoening(Autisme);
-        twee.setAandoening(Hazenlip);
-        System.out.println(een.toString());
-        System.out.println(twee.toString());
+        System.out.println(autisme.equals(hazenlip));
+        System.out.println(autisme.equals(autisme));
 
-        System.out.println(twee.getLeeftijdsverwachting());
-        System.out.println(een.getLeeftijdsverwachting());
-        System.out.println(twee.getKwaliteitVanLeven());
-        System.out.println(een.getKwaliteitVanLeven());
+        babyEen.setAandoening(hersenvliesontsteking);
+        babyTwee.setAandoening(autisme);
+        babyTwee.setAandoening(hazenlip);
+        puberEen.setAandoening(rinovirus);
+
+
+        System.out.println(babyEen.toString());
+        System.out.println(babyTwee.toString());
+
+        System.out.println(babyEen.babyCry());
+        System.out.println(babyEen.Yearpassed());
+        System.out.println(babyTwee.Yearpassed());
+        System.out.println(kindEen.Yearpassed());
+        System.out.println(kindEen.buitenSpelen(rinovirus));
+
+        System.out.println(puberEen.gaatNaarFestival());
+        System.out.println(puberTwee.gaatNaarFestival());
+
 
 
 //        Aandoeningen Alzheimer = new Aandoeningen("Alzheimer", 10, 4);
