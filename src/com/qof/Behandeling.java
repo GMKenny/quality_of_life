9d8434405008e1b8b574d7caf92fc5573676a0a8
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-25 14:58:48 +0200

    Class Behandeling toegevoegd
    Omschrijving 3 functies.
    1. Behandeling is een constructor voor de class met 1 persoon voor behandeling.
    2. AddPatient retourneerd een string of de patient toegevoegd kan worden aan de behandeling. er word gecontroleerd of de patient een aandoenig heeft en of de lijst all dezelfde patien bevat.
    3. Patienteninformatie retourneerd een string met alle patienten die nu onder behandeling zijn. als er geen patienten in behandeling zijn word dat opgevangen.

---
+++ src/com/qof/Behandeling.java
@@ -0,0 +1,41 @@
+package com.qof;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Behandeling {
+    private final Set<Patiënt> Patiënten = new HashSet<>();
+    private final int MaxPatients = 10;
+
+    public Behandeling(Patiënt patiënt){
+        if (patiënt.heeftAandoenigen()){
+            Patiënten.add(patiënt);
+        }
+    }
+
+    public String addPatiënt(Patiënt patiënt){
+        if (patiënt.heeftAandoenigen()){
+            if (!Patiënten.contains(patiënt)){
+                Patiënten.add(patiënt);
+                return "De Volgende patiënt begint nu aan zijn behandeling:\n" + patiënt.toString();
+            }
+            return "De Volgende patiënt word al behandeld:\n" + patiënt.toString();
+        }
+        return "De Volgende Patient heeft geen aandoeningen:\n" + patiënt.toString();
+    }
+
+    public String PatientenInformatie(){
+        String informatie = "De volgende patienten zijn nu onder behandeling:\n";
+        int counter = 1;
+        for (Patiënt patiënt : Patiënten){
+            informatie += "Patiënt: " + counter + ".\n" + patiënt.toString();
+            counter += 1;
+        }
+        if (Patiënten.isEmpty()){
+            informatie = "Er zijn op het moment geen patiënten in behandeling:";
+        }
+        return  informatie;
+    }
+
+
+}
