ff4cf0995a947e81af4d870ca50ec274e830828f
GMKenny <kenny.vandenberg@student.hu.nl>
2021-04-25 15:00:25 +0200

    Main Aangepast
    Voor de test worden er een aantal  verschilende functies aangeroepen om te laten zien dat alles werkt.

--- src/com/qof/Main.java
+++ src/com/qof/Main.java
@@ -8,7 +8,12 @@ public class Main {
         Kind kindEen = new Kind( "Kees", "Man", 1, Leeftijdcategory.Kind);
         Puber puberEen = new Puber("Clara", "vrouw", 17, Leeftijdcategory.Puber);
         Puber puberTwee = new Puber("Jan", "Man", 18, Leeftijdcategory.Puber);
-
+        Volwassen volwassenEen = new Volwassen("Johan", "Man", 27, Leeftijdcategory.Volwassen);
+        Volwassen volwassenTwee = new Volwassen("Sara", "Vrouw", 34, Leeftijdcategory.Volwassen);
+        Volwassen volwassenDrie = new Volwassen("Mitchel", "Man", 31, Leeftijdcategory.Volwassen);
+        Volwassen volwassenVier = new Volwassen("Lieke", "Vrouw", 29, Leeftijdcategory.Volwassen);
+        Senior seniorEen = new Senior("Henk", "Man", 61, Leeftijdcategory.Senior);
+        Senior seniorTwee = new Senior("Bianca", "Vrouw", 61, Leeftijdcategory.Senior);
 
         Aandoeningen autisme = new Aandoeningen("Autisme", 16, 7.5, 7, false, false);
         Aandoeningen hazenlip = new Aandoeningen("Hazenlip", 0, 9.99, 10, false, false);
@@ -19,12 +24,18 @@ public class Main {
 
         System.out.println(autisme.equals(hazenlip));
         System.out.println(autisme.equals(autisme));
+        System.out.println(seniorTwee.equals(seniorEen));
+        System.out.println(kindEen.equals(kindEen));
+        System.out.println(puberTwee.equals(puberEen));
 
         babyEen.setAandoening(hersenvliesontsteking);
         babyTwee.setAandoening(autisme);
         babyTwee.setAandoening(hazenlip);
         puberEen.setAandoening(rinovirus);
-
+        volwassenEen.setAandoening(chlamydia);
+        volwassenTwee.setAandoening(trichomonas);
+        volwassenDrie.setAandoening(hersenvliesontsteking);
+        seniorEen.setAandoening(hersenvliesontsteking);
 
         System.out.println(babyEen.toString());
         System.out.println(babyTwee.toString());
@@ -34,14 +45,21 @@ public class Main {
         System.out.println(babyTwee.Yearpassed());
         System.out.println(kindEen.Yearpassed());
         System.out.println(kindEen.buitenSpelen(rinovirus));
-
         System.out.println(puberEen.gaatNaarFestival());
         System.out.println(puberTwee.gaatNaarFestival());
+        System.out.println(seniorEen.kennisGeven(volwassenVier));
+        System.out.println(seniorEen.Yearpassed());
+        System.out.println(volwassenEen.relaatsiesMetElkaar(volwassenTwee));
+        System.out.println(volwassenDrie.relaatsiesMetElkaar(volwassenVier));
+        System.out.println(volwassenTwee.relaatsiesMetElkaar(volwassenVier));
+        System.out.println(volwassenVier.toString());
+        System.out.println(volwassenDrie.toString());
 
-
-
-//        Aandoeningen Alzheimer = new Aandoeningen("Alzheimer", 10, 4);
-//        System.out.println(Alzheimer.toString());
-
+        Behandeling behandelingEen = new Behandeling(kindEen);
+        System.out.println(behandelingEen.addPatiënt(kindEen));
+        System.out.println(behandelingEen.addPatiënt(babyEen));
+        System.out.println(behandelingEen.addPatiënt(volwassenVier));
+        System.out.println(behandelingEen.addPatiënt(volwassenDrie));
+        System.out.println(behandelingEen.PatientenInformatie());
     }
 }
